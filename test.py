dd445b764eed5d3c4fcbce37b424f9284fce41e8
flocuterr <zzy2695980257@163.com>
2025-10-06 00:08:59 +0800

    second pull

--- test.py
+++ test.py
@@ -4,17 +4,22 @@
 #     count_str = str(count) if count else 'no'
 #     return f'{count_str} {word}s'
 
-def show_count(count: int,singular:str, plural: str='') -> str:
-    if count == 1:
-        return f'1 {singular}'
-    count_str = str(count) if count else 'no'
-    if plural:
-        return f'{count} {plural}'
-    return f'{count_str} {singular}s'
+# def show_count(count: int,singular:str, plural: str='') -> str:
+#     if count == 1:
+#         return f'1 {singular}'
+#     count_str = str(count) if count else 'no'
+#     if plural:
+#         return f'{count} {plural}'
+#     return f'{count_str} {singular}s'
 
-def test_irregular() -> None:
-    got = show_count(2,'child','children')
-from collections import abc
+# def test_irregular() -> None:
+#     got = show_count(2,'child','children')
+# from collections import abc
 
-def double(x: abc.Sequence):
-    return x * 2
+# def double(x: abc.Sequence):
+#     return x * 2
+
+def parse_token(token: str) -> str | float:
+    if len(token):
+        return f'{token}'
+    return 0
